9c0f3f5931e19a0b6a87a8c82272d618d133e46f
Fabien Plastina <fplastina@localhost.localdomain>
2016-12-09 18:22:05 +0100

    quasiment fini le statique, il manque juste l'ajout de la gestion de la taille de l'image

--- statique/fonction.c
+++ statique/fonction.c
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "statique.h"
 //[]
 
 void goTo(int x, int y, int lap)
@@ -22,4 +22,48 @@ void goTo(int x, int y, int lap)
 	{
 		printf (" ");
 	}
+}
+
+int *taille (char *ligne)
+{
+	char taillex[5];
+	char tailley[5];
+	int *sizeImg[2];
+	int i;
+	int j;
+
+	for (i=0; i<50; i++)
+	{	
+		if (ligne[i] == ' ')
+		{
+			j = i;
+			i = 43;
+		}
+		
+		else
+		{
+			taillex[i] = ligne[i];
+		}
+	}
+
+	i = 0;
+		
+	while (j<50)
+	{	
+		if (ligne[j] == ' ' || ligne[j] == '\0' || ligne[j] == '\n')
+		{
+			j = 43;
+		}
+		
+		else
+		{
+			taillex[i] = ligne[j];
+		}
+		
+	i ++;
+	j ++;
+	}
+	
+	sizeImg[0] = atoi(taillew);
+	sizeImg[1] = atoi(tailley);
 }
--- statique/statique.c
+++ statique/statique.c
@@ -1,4 +1,4 @@
-#include "header.h"
+#include "statique.h"
 //[]
 
 void main()
@@ -8,29 +8,39 @@ void main()
 	int i = 88;
 	int j;
 	int lap = 0;
-	int x = 60;
-	int y = 1;
+	int posx = 60;
+	int posy = 1;
+	int nbligne = 4;
 	char ligne[255];
 	char touche;
 	FILE *file = fopen ("voiture.pbm","r");
+	int *sizeImg[2];
 	
 	while (fgets (ligne, 255, file)!= NULL)
 	{
-	goTo(x, y, lap);
-		for (j=0; j<255; j++)
-		{
-			if (ligne[j] == '0')
-			{
-				printf("%c", i);
-				lap = 1;
-			}
 		
-			if (ligne[j] == '1')
+		if (nbligne == 3)
+		{
+			sizeImg = taille(ligne);
+		}		
+			
+		if (nbligne >3)
+		{	
+			for (j=0; j<255; j++)
 			{
-				printf (" ");
+				if (ligne[j] == '0')
+				{
+					printf("%c", i);
+					lap = 1;
+				}
+			
+				if (ligne[j] == '1')
+				{
+					printf (" ");
+				}
 			}
 		}
-	
+	nbligne ++;
 	}
 	
 	printf("\n");
