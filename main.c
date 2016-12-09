b1a5764da2d74eaaa9c9e7aec90c8058a7a94ae1
Fabien Plastina <fplastina@localhost.localdomain>
2016-12-09 10:51:08 +0100

    ajout de la fonction pour centrer une image ainsi que le header

--- main.c
+++ main.c
@@ -1,38 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "header.h"
 //[]
 
-int main()
+void main()
 {
 	system ("clear");	
 
 	int i = 88;
 	int j;
-	int x = 20;
+	int lap = 0;
+	int x = 80;
 	int y = 20;
 	char ligne[255];
 	FILE *file = fopen ("ex1.pbm","r");
-
-	void goto_x_y(unsigned x, unsigned y)
-	{
 	
-		while (fgets (ligne, 255, file)!= NULL)
+	while (fgets (ligne, 255, file)!= NULL)
+	{
+	goTo(x, y, lap);
+		for (j=0; j<80; j++)
 		{
-			for (j=0; j<80; j++)
+			if (ligne[j] == '1')
 			{
-				if (ligne[j] == '1')
-				{
-					printf("%c\033[%u;%uH", i, y, x);
-				}
-			
-				if (ligne[j] == '0')
-				{
-					printf (" \033[%u;%uH", y, x);
-				}
+				printf("%c", i);
+				lap = 1;
 			}
-		printf("\n");
+		
+			if (ligne[j] == '0')
+			{
+				printf (" ");
+			}
 		}
+	
 	}
+
 
 	fclose(file);	
 }
