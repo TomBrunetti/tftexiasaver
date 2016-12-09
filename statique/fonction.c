4af504262f789fed958e6d19e28181b38a0e0ee7
Fabien Plastina <fplastina@localhost.localdomain>
2016-12-09 13:22:51 +0100

    création du dossier "statique"

---
+++ statique/fonction.c
@@ -0,0 +1,25 @@
+#include "header.h"
+//[]
+
+void goTo(int x, int y, int lap)
+{
+	int a, b;
+
+	if (lap == 0)
+	{	
+		for (b=0; b < y; b++)
+		{
+			printf("\n");
+		}
+	}
+	
+	else
+	{
+		printf ("\n");
+	}
+	
+	for (a=0; a < x; a++)
+	{
+		printf (" ");
+	}
+}
