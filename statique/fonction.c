e5b370b84e4220f23ddca1a99ba23dbaf6ae4297
Fabien Plastina <fplastina@localhost.localdomain>
2016-12-12 13:44:49 +0100

    petite séance de déboguage

--- statique/fonction.c
+++ statique/fonction.c
@@ -1,77 +1,144 @@
 #include "statique.h"
 //[]
 
-void goTo(int x, int y, int lap) // Fonction qui place l'image, ne retourne rien mais prends des coordonnées et "l'activateur" en entrée
+void affichage (int posx, int posy, FILE *file)
+ // Affichage d'une image
 {
-	int a, b; // On déclare a et b
+	int a, j;
+ // On déclare a, compteur de la boucle de longueur et j, 2 ème boucle de la récursive
+	int i = 88;
+ // Code ascii du X
+	int nbligne = 10;
+ // Compteur de ligne du fichier
+	char ligne[255];
+ // String où on stocke la ligne du fichier
 
-	if (lap == 0) // tant que lap, donc l'activateur, est vérifié
-	{	
-		for (b=0; b < y; b++) // tant qu'on n'est pas à la ligne demandé en argument
-		{
-			printf("\n"); // On met un retour à la ligne
-		}
-	}
-	
-	else
-	{
-		printf ("\n"); // sinon on met un retour à la ligne
-	}
+	for (j=0; j<posy; j++){printf("\n");}
+ // Nous place au bon cacactère
 
-/* Ok j'avoue que le truc au dessus est pas très clair
- * même moi je comprends pas bien donc pour l'instant sa marche
- * mais je modiefierais plus tard donc faites y pas trop attention
- */
-	
-	for (a=0; a < x; a++) // Tant qu'on est pas au caractère demandé
-	{
-		printf (" "); // On imprime un espace
-	}
+        while (fgets (ligne, 255, file)!= NULL)
+ // Tant que la ligne du fichier lue n'est pas la dernière
+        {
+                if (nbligne >3)
+ // Si on a dépassé la troisième ligne, donc on est dans l'image en soi
+                {
+			for (a=0; a < posx; a++){printf (" ");}
+ // On imprime un espace tant qu' n est pas au caractère demandé
+
+                        for (j=0; j<posx; j++)
+ // On parcourt chaque caractère de la ligne
+                        {
+                                if (ligne[j] == '0')
+ // Si c'est un 0
+                                {
+                                        printf("%c", i);
+ // On affiche en char le code ascii rentré dans i. Donc on affiche un X
+                                }
+
+                                if (ligne[j] == '1')
+ // Si c'est un 1
+                                {
+                                        printf (" ");
+ // On affiche un espace, donc un pixel noir
+                                }
+                        }
+                }
+
+	        printf("\n");
+ // On affiche un retour à la ligne		
+        }
 }
 
-void taille (char *ligne, int *sizeImg) // Fonction qui relève la taille donnée dans le fichier, ne renvoie rien puisque les tableaux marchent comme des pointeurs, prends la ligne du doc et le tableau en argument
+
+
+
+
+
+//------------------------------------------------------------------------------------//
+
+
+
+
+
+
+void taille (FILE *file, int *sizeImg)
+ // Fonction qui relève la taille donnée dans le fichier, ne renvoie rien puisque les tableaux marchent comme des pointeurs, prends le doc et le tableau en argument
 {
-	char taillex[5]; // String de la longueur
-	char tailley[5]; // String de la hauteur
-	int i, j, x, y; /* Déclaration de 4 entiers:
+	char taillex[5];
+ // String de la longueur
+	char tailley[5];
+ // String de la hauteur
+	char ligne[255];
+	int i, j, x, y, nbligne;
+ /* Déclaration de 4 entiers:
 i est le compteur pour la boucle de la longueur
 j est le compteur de la boucle de la hauteur
 x et la longueur
 y est la hauteur */
-	for (i=0; i<50; i++) // Tant que i vaut moins que 50 (valeur un peu aléatoire)
-	{	
-		if (ligne[i] == ' ') // Si le caractère n'est pas un chiffre
-		{
-			x = i; // On sauvegarde la valeur de i
-			i = 143; // On casse la boucle
-		}
-		
-		else // Sinon (si c'est un chiffre)
-		{
-			taillex[i] = ligne[i]; // On stocke le chiffre dans la string de la longueur
-		}
-	}
 
-	i = 0; // On reset i
-	
-	for (j = x + 1; j<50; j++) // Tant que j est inférieur à 50 (là aussi c'est du random)
-	{	
-		if (ligne[j] == ' ' || ligne[j] == '\0' || ligne[j] == '\n') // Si le caractère n'est pas un chiffre
-		{
-			j = 143; // On casse la boucle
-		}
-		
-		else // Sinon (si c'est un chiffre)
-		{
-			tailley[i] = ligne[j]; // On stocke le chiffre dans la string de la hauteur
-		}
-		
-	i ++; // On incrémente i, qui est ici la position dans la string de la hauteur
-	}
-	
-	x = atoi(taillex); // On converti en int la string de la longueur et on la stock dans x
-	y = atoi(tailley); // Pareil pour la string de la hauteur et on la stock dans y
+        while (fgets (ligne, 255, file)!= NULL)
+ // Tant que la ligne du fichier lue n'est pas la dernière
+        {
+		nbligne ++;
 
-	sizeImg[0] = x; // On met la longueur en 1ère position...
-	sizeImg[1] = y; // ... et la hauteur en 2ème
+                if (nbligne == 3)
+ // Si c'est la troisième ligne
+                {
+              	
+			for (i=0; i<50; i++)
+ // Tant que i vaut moins que 50 (valeur un peu aléatoire)
+			{	
+				if (ligne[i] == ' ')
+ // Si le caractère n'est pas un chiffre
+				{	
+					x = i;
+ // On sauvegarde la valeur de i
+					i = 143;
+ // On casse la boucle
+				}
+		
+				else
+ // Sinon (si c'est un chiffre)
+				{
+					taillex[i] = ligne[i];
+ // On stocke le chiffre dans la string de la longueur
+				}
+			}
+		}
+
+			i = 0;
+ // On reset i
+	
+			for (j = x + 1; j<50; j++)
+ // Tant que j est inférieur à 50 (là aussi c'est du random)
+			{	
+				if (ligne[j] == ' ' || ligne[j] == '\0' || ligne[j] == '\n')
+ // Si le caractère n'est pas un chiffre
+				{
+					j = 143;
+ // On casse la boucle
+				}
+		
+				else
+ // Sinon (si c'est un chiffre)
+				{
+					tailley[i] = ligne[j];
+ // On stocke le chiffre dans la string de la hauteur
+				}
+		
+				i ++;
+ // On incrémente i, qui est ici la position dans la string de la hauteur
+			}
+	
+			x = atoi(taillex);
+ // On converti en int la string de la longueur et on la stock dans x
+			y = atoi(tailley);
+ // Pareil pour la string de la hauteur et on la stock dans y
+
+			sizeImg[0] = x;
+ // On met la longueur en 1ère position...
+			sizeImg[1] = y;
+ // ... et la hauteur en 2ème
+			fclose (file);
+	}
 }
--- statique/statique.c
+++ statique/statique.c
@@ -3,59 +3,42 @@
 
 void main()
 {
-	system ("clear"); // Nettoie la console	
+	system ("clear");
+ // Nettoie la console	
 
-	int i = 88; // Code ascii du X
-	int j, posx, posy; // j = Compteur de la deuxième boucle, posx et poy = coordonnées de l'image
-	int lap = 0; // Variable qui arrète les retours à la ligne 
-	int nbligne = 0; // Compteur de ligne du fichier
-	char ligne[255]; // String où on stocke la ligne du fichier
-	char touche; // UTILE ?
-	FILE *file = fopen ("voiture.pbm","r"); // On ouvre l'image de format ".pbm"
-	int sizeImg[2]; // Taille de l'image
-	
-	while (fgets (ligne, 255, file)!= NULL) // Tant que la ligne du fichier lue n'est pas la dernière
-	{
+	int posx, posy;
+ // posx et poy = coordonnées de l'image
+	char ligne[255];
+ // String où on stocke la ligne du fichier
+	char touche;
+ // UTILE ?
+	FILE *file = fopen ("ex1.pbm","r");
+ // On ouvre l'image de format ".pbm"
+	int sizeImg[2];
+ // Taille de l'image
 		
-		if (nbligne == 3) // Si c'est la troisième ligne
-		{
-			sizeImg[lap] = taille(ligne, lap); // On appelle la fonction taille pour la longueur de l'image
-			posx = (178 - sizeImg[0]) / 2; // On stocke la longueur dans posx
-			lap ++; // On incrémente lap pour changer de place dans le tableau sizeImg
-			sizeImg[lap] = taille(ligne, lap); // Pareil pour la hauteur
-			posy = (54 - sizeImg[1]) / 2; // On stocke la hauteur dabns posy
-			lap = 0; // On reset lap pour la suite
-		}		
-			
-		if (nbligne >3) // Si on a dépassé la troisième ligne, donc on est dans l'image en soi
-		{
-		goTo(posx, posy, lap); // On appelle la fonction goTo pour placer l'image
-			for (j=0; j<255; j++) // On parcourt chaque caractère de la ligne
-			{
-				if (ligne[j] == '0') // Si c'est un 0
-				{
-					printf("%c", i); // On affiche en char le code ascii rentré dans i. Donc on affiche un X
-					lap = 1; // On interrompt les \n consécutifs
-				}
-			
-				if (ligne[j] == '1') // Si c'est un 1
-				{
-					printf (" "); // On affiche un espace, donc un pixel noir
-				}
-			}
-		}
-	nbligne ++; // On incrémente le compteur de ligne
-	}
-	
-	printf("\n"); // On affiche un retour à la ligne
+	taille (file, sizeImg); 
+	posx = (80 - sizeImg[0]) / 2;
+ // On stocke la longueur dans posx
+	posy = (24 - sizeImg[1]) / 2;
+ // On stocke la hauteur dabns posy
+	printf ("posx = %d, posy = %d", sizeImg[0], sizeImg[1]);
 
-	system ("/bin/stty raw"); // On passe en mode "raw" pour vider automatiquement le tampon
-	while (!touche) // Tant qu'aucune touche n'est activée
+	affichage (posx, posy, file);
+
+	system ("/bin/stty raw");
+ // On passe en mode "raw" pour vider automatiquement le tampon
+	while (!touche)
+ // Tant qu'aucune touche n'est activée
 	{
-		touche = getchar(); // On relève la touche
+		touche = getchar();
+ // On relève la touche
 	}
 	
-	system ("/bin/stty cooked"); // On repasse en mode "cooked" = normal
-	system ("clear"); // On renettoie la console
-	fclose (file); // On ferme le fichier
+	system ("/bin/stty cooked");
+ // On repasse en mode "cooked" = normal
+	system ("clear");
+ // On renettoie la console
+	fclose (file);
+ // On ferme le fichier
 }
