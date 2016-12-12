75c11cec1b3cce4bc28d7646675ee37d08bd32eb
Fabien Plastina <fplastina@localhost.localdomain>
2016-12-12 10:13:30 +0100

    Commentires des 2 fonctions goTo et taille

--- statique/fonction.c
+++ statique/fonction.c
@@ -1,72 +1,81 @@
 #include "statique.h"
 //[]
 
-void goTo(int x, int y, int lap)
+void goTo(int x, int y, int lap) // Fonction qui place l'image, ne retourne rien mais prends des coordonnées et "l'activateur" en entrée
 {
-	int a, b;
+	int a, b; // On déclare a et b
 
-	if (lap == 0)
+	if (lap == 0) // tant que lap, donc l'activateur, est vérifié
 	{	
-		for (b=0; b < y; b++)
+		for (b=0; b < y; b++) // tant qu'on n'est pas à la ligne demandé en argument
 		{
-			printf("\n");
+			printf("\n"); // On met un retour à la ligne
 		}
 	}
 	
 	else
 	{
-		printf ("\n");
+		printf ("\n"); // sinon on met un retour à la ligne
 	}
+
+/* Ok j'avoue que le truc au dessus est pas très clair
+ * même moi je comprends pas bien donc pour l'instant sa marche
+ * mais je modiefierais plus tard donc faites y pas trop attention
+ */
 	
-	for (a=0; a < x; a++)
+	for (a=0; a < x; a++) // Tant qu'on est pas au caractère demandé
 	{
-		printf (" ");
+		printf (" "); // On imprime un espace
 	}
 }
 
-int taille (char *ligne, int loop)
+int taille (char *ligne, int loop) // Fonction qui relève la taille donnée dans le fichier, renvoie un entier, prends la ligne du doc et un compteur de tour en arg
 {
-	char taillex[5];
-	char tailley[5];
-	int sizeImg[2];
-	int i, j, x, y;
+	char taillex[5]; // String de la longueur
+	char tailley[5]; // String de la hauteur
+	int sizeImg[2]; // Tableau de taille en entier
+	int i, j, x, y; /* Déclaration de 4 entiers:
+i est le compteur pour la boucle de la longueur
+j est le compteur de la boucle de la hauteur
+x et la longueur
+y est la hauteur */
 
-	for (i=0; i<50; i++)
+	for (i=0; i<50; i++) // Tant que i vaut moins que 50 (valeur un peu aléatoire)
 	{	
-		if (ligne[i] == ' ')
+		if (ligne[i] == ' ') // Si le caractère n'est pas un chiffre
 		{
-			j = i;
-			i = 43;
+			x = i; // On sauvegarde la valeur de i
+			i = 143; // On casse la boucle
 		}
 		
-		else
+		else // Sinon (si c'est un chiffre)
 		{
-			taillex[i] = ligne[i];
+			taillex[i] = ligne[i]; // On stocke le chiffre dans la string de la longueur
 		}
 	}
 
-	i = 0;
+	i = 0; // On reset i
 		
-	for (j = 0; j<50; j++)
+	for (j = x; j<50; j++) // Tant que j est inférieur à 50 (là aussi c'est du random)
 	{	
-		if (ligne[j] == ' ' || ligne[j] == '\0' || ligne[j] == '\n')
+		if (ligne[j] == ' ' || ligne[j] == '\0' || ligne[j] == '\n') // Si le caractère n'est pas un chiffre
 		{
-			j = 43;
+			j = 143; // On casse la boucle
 		}
 		
-		else
+		else // Sinon (si c'est un chiffre)
 		{
-			tailley[i] = ligne[j];
+			tailley[i] = ligne[j]; // On stocke le chiffre dans la string de la hauteur
 		}
 		
-	i ++;
+	i ++; // On incrémente i, qui est ici la position dans la string de la hauteur
 	}
 	
-	x = atoi(taillex);
-	y = atoi(tailley);
+	x = atoi(taillex); // On converti en int la string de la longueur et on la stock dans x
+	y = atoi(tailley); // Pareil pour la string de la hauteur et on la stock dans y
 
-	sizeImg[0] = x;
-	sizeImg[1] = y;
+	sizeImg[0] = x; // On met la longueur en 1ère position...
+	sizeImg[1] = y; // ... et la hauteur en 2ème
 
-	return sizeImg[loop];
+	return sizeImg[loop]; // On return la valeur de la longueur ou de la hauteur selon la valeur de loop
 }
