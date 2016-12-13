f69b52f3a493ffb605a1cd94a3bee4ffc0b2820f
Fabien Plastina <fplastina@localhost.localdomain>
2016-12-13 10:41:31 +0100

    ajout des commentaires de l'affichage statique

--- statique/statique.c
+++ statique/statique.c
@@ -1,32 +1,41 @@
 #include "statique.h"
+// On inclut le header
 #define fichier "voiture.pbm"
+// On cree une valeur globale pour changer l'image a cet endroit seulement
 
-int main(int argc, char *argv[])
-
+void main()
+// Fonction principale
 {
-system("clear");
+	system("clear");
+// On nettoie l'ecran
 
-FILE *file = fopen(fichier, "r");
-int x, y;
+	FILE *file = fopen(fichier, "r");
+// On ouvre le fichier en lecture seule
+	int x, y;
+// Declaration de x et y, position de l'image a l'ecran
 
-taille(file, &x, &y);
-fclose(file);
+	taille(file, &x, &y);
+// On appelle la fonction taille avec comme arguments le fichier et les pointeurs de x et y
+	fclose(file);
+// On ferme le fichier
 
-file = fopen(fichier, "r");
-afficher(file, x, y);
-system("/bin/stty raw");
+	file = fopen(fichier, "r");
+// On le rouvre
+	afficher(file, x, y);
+// On fait appelle a la fonction afficher avec comme argument le fichier et la taille de l'image
 
-while(!getchar())
-{printf("");
+	system("/bin/stty raw");
+// On passe le system en mode "raw" afin de vider le tampon automatiquement
+	
+	while(!getchar()){}
+// Tant qu'aucune touche n'est actionnee (creation d'une boucle infinie)
 
-}
+	system("/bin/stty cooked");
+// On repasse le systeme en mode "cooked", le mode normal donc
 
-system("/bin/stty cooked");
+	system ("clear");
+// On renettoie l'ecran
 
-system ("clear");
-
-fclose(file);
-
-return 0;
-
+	fclose(file);
+// On ferme le fichier
 }
