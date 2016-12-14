a2b8ff7131c2f63deb6115dc8054797dfbded414
Fabien Plastina <fplastina@localhost.localdomain>
2016-12-14 14:47:55 +0100

    Ajout des statistiques

--- prog/main.c
+++ prog/main.c
@@ -11,40 +11,49 @@ int main(int argc, char *argv[])
 	time_t sec;
 	struct tm temps;
 	char infolog[255];
+	char stat[6] = "-stat"; 
 	int random = alea(1,3);
 
-	time(&sec);
-	temps = *localtime(&sec);
-
-	switch (random)
-// Fonction qui lance le programme correspondant au nombre choisi (1 pour statique, 2 pour dynamique et 3 pour interactif
+	if (argv[1] != NULL)
 	{
+		if(strcmp(argv[1], stat) == 0){system("vim log");}
+	}	
 
-		case 1:
-			system("./statique.out");
+	else
+	{
+		time(&sec);
+		temps = *localtime(&sec);
+
+		sprintf(infolog, "%d/%d/%d %d:%d:%d;%d;\n",temps.tm_mday + 1, temps.tm_mon + 1, temps.tm_year, temps.tm_hour, temps.tm_min, temps.tm_sec, random);
+		fputs(infolog, log);	
+
+		switch (random)
+// Fonction qui lance le programme correspondant au nombre choisi (1 pour statique, 2 pour dynamique et 3 pour interactif
+		{
+
+			case 1:
+				execl("/home/fplastina/git/projet/tftexiasaver/prog/statique.out", NULL);//system("./statique.out");//exec("./statique.out");;
 // si choixEcran vaut 1 alors le programme choisis l'écran statique
-		break;
+			break;
 // Le programme fait un break
 
-		case 2:
-			printf("dynamique.out\n");//system("./dynamique.out");
+			case 2:
+				printf("dynamique.out\n");//system("./dynamique.out");
 // si choixEcran vaut 2 alors le programme choisis l'ecran dynamique
-		break;
+			break;
 
-		case 3:
-			printf("interactif.out\n");//system("./interactif.out");
+			case 3:
+				printf("interactif.out\n");//system("./interactif.out");
 // si choixEcran vaut 3 alors le programme choisis l'ecran interactif
-		break;
+			break;
 
-		defaut:
-			printf("Erreur dans le choix du programme\n");
+			defaut:
+				printf("Erreur dans le choix du programme\n");
 // si aucun des 3 nombre n'est choisis alors le programme affiche une erreur
-		break;
+			break;
 
+		}
 	}
-
-	sprintf(infolog, "%d/%d/%d %d:%d:%d;%d;\n",temps.tm_mday + 1, temps.tm_mon + 1, temps.tm_year, temps.tm_hour, temps.tm_min, temps.tm_sec, random);
-	fputs(infolog, log);	
 
 	return 0;
 // le main renvoie la valeur 0 pour dire que tout c'est bien passé
--- prog/statique.h
+++ prog/statique.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 void afficher(FILE *, int, int);
 void taille(FILE *, int*, int*);
